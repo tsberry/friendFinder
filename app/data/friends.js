4d977dffc0045582b1aeba1295269f529521cee1
unknown <tberry6@gmail.com>
2018-04-16 18:25:04 -0700

    Added a matching function

--- app/data/friends.js
+++ app/data/friends.js
@@ -23,7 +23,39 @@ function addFriend(friend, callback) {
     })
 }
 
+function bestMatch(name, callback) {
+    fs.readFile("./app/data/friends.json", "utf8", function(err, data) {
+        if(err) throw err;
+        var friends = JSON.parse(data).friends;
+        for(var i = 0; i < friends.length; i++) {
+            if(friends[i].name === name) {
+                scores = friends[i].name;
+                var min;
+                var minDiff = 200;
+                for(var i = 0; i < friends.length; i++) {
+                    if(friends[i].name !== name) {
+                        if(difference(friends[i], scores) < minDiff) {
+                            minDiff = difference(friends[i], scores);
+                            min = friends[i];
+                        }
+                    }
+                }
+            }
+        }
+        callback(min);
+    });
+}
+
+function difference(a, b) {
+    var difference = 0;
+    for(var i = 0; i < a.length; i++) {
+        difference += Math.abs(a[i] - b[i]);
+    }
+    return difference;
+}
+
 module.exports = {
     getFriends: getFriends,
-    addFriend: addFriend
+    addFriend: addFriend,
+    bestMatch: bestMatch
 }
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,7 @@
 var friends = require("../data/friends.js");
 var getFriends = friends.getFriends;
 var addFriend = friends.addFriend;
+var bestMatch = friends.bestMatch;
 var bodyParser = require("body-parser");
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
@@ -18,7 +19,7 @@ apiRoutes = {
             for(property in req.body) {
                 if(property !== "name") arr.push(parseInt(req.body[property]));
             }
-            addFriend({"name": name, "scores": arr}, function(data) {res.send(data);});
+            addFriend({"name": name, "scores": arr}, function(data) {bestMatch(name, function(data) {res.send(data);});});
         });
     }
 };
