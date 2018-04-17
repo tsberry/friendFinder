05f75a5423709206f42feaf8c0a80fe177e9a5c6
unknown <tberry6@gmail.com>
2018-04-16 17:53:55 -0700

    Wrote post route

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,29 @@
-var friends = [];
+var fs = require("fs");
 
-module.exports = friends;
+function getFriends(callback) {
+    fs.readFile("./app/data/friends.json", "utf8", function(err, data) {
+        if(err) throw err;
+        else {
+            callback(JSON.parse(data));
+        }
+    });
+}
+
+function addFriend(friend, callback) {
+    fs.readFile("./app/data/friends.json", "utf8", function(err, data) {
+        if(err) throw err;
+        else {
+            var obj = JSON.parse(data);
+            obj.friends.push(friend);
+            fs.writeFile("./app/data/friends.json", JSON.stringify(obj), "utf8", function(err) {
+                if(err) throw err;
+                callback("Added friend!");
+            });
+        }
+    })
+}
+
+module.exports = {
+    getFriends: getFriends,
+    addFriend: addFriend
+}
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,13 +1,16 @@
 var friends = require("../data/friends.js");
+var getFriends = friends.getFriends;
+var addFriend = friends.addFriend;
 
 apiRoutes = {
     getRoute: function(app) {
         app.get("/api/friends", function(req, res) {
-            res.json(friends);
+            getFriends(function(data) {res.send(data);});
         });
     },
     postRoute: function(app) {
-        app.post("/api/friends", function(req, res) {
+        app.post("/api/friends/submit", function(req, res) {
+            addFriend({"name" : "Thomas3"}, function(data) {res.send(data);});
         });
     }
 };
